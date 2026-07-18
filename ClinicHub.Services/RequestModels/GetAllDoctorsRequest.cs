11864645aef9ea4f225a1fdaad77c4d078b65dd7
Mohamed Saber <mmms77990@gmail.com>
2026-07-18 04:59:28 +0300

    updates

--- ClinicHub.Services/RequestModels/GetAllDoctorsRequest.cs
+++ ClinicHub.Services/RequestModels/GetAllDoctorsRequest.cs
@@ -2,9 +2,9 @@ namespace ClinicHub.Services.RequestModels
 {
     public class GetAllDoctorsRequest
     {
-        public string? SearchTerm { get; set; }
-        public bool? IsUnassigned { get; set; }
-        public int PageNumber { get; set; } = 1;
-        public int PageSize { get; set; } = 20;
-    }
+    public string? SearchTerm { get; set; }
+    public bool? IsUnassigned { get; set; }
+    public int PageNumber { get; set; } = 1;
+    public int PageSize { get; set; } = 20;
+}
 }
--- ClinicHub.Services/Services/Implementations/DoctorService.cs
+++ ClinicHub.Services/Services/Implementations/DoctorService.cs
@@ -2,28 +2,38 @@ using ClinicHub.Services.Contracts;
 using ClinicHub.Services.Exceptions;
 using ClinicHub.Services.ReponseModels;
 using ClinicHub.Services.RequestModels;
+using Microsoft.AspNetCore.Http;
 
 namespace ClinicHub.Services.Services.Implementations
 {
     public class DoctorService : IDoctorService
     {
         private readonly IUserService _userService;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public DoctorService(IUserService userService)
+        public DoctorService(IUserService userService, IHttpContextAccessor httpContextAccessor)
         {
             _userService = userService;
+            _httpContextAccessor = httpContextAccessor;
         }
 
         public async Task<PagginatedResult<UserResponseDto>> GetAllDoctorsPagginatedAsync(GetAllDoctorsRequest request)
         {
             try
             {
+                var hasClinicFilter = _httpContextAccessor.HttpContext?.Items["ClinicId"] is Guid;
+                var isFiltering = request.IsUnassigned is true || hasClinicFilter;
+
+                var userTypes = new List<Enums.UserType> { Enums.UserType.Doctor };
+                if (!isFiltering)
+                    userTypes.Add(Enums.UserType.ClinicOwner);
+
                 var usersRequest = new GetAllUsersRequest
                 {
                     PageNumber = request.PageNumber,
                     PageSize = request.PageSize,
                     SearchTerm = request.SearchTerm,
-                    UserTypes = new() { Enums.UserType.Doctor, Enums.UserType.ClinicOwner },
+                    UserTypes = userTypes,
                     IsUnassigned = request.IsUnassigned
                 };
 
