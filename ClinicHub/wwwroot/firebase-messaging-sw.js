8f3b7d6b1d5e27f04b7743093a9531ae5b65d888
Mohamed Saber <mmms77990@gmail.com>
2026-08-07 19:26:20 +0300

    savechanges

--- ClinicHub/wwwroot/firebase-messaging-sw.js
+++ ClinicHub/wwwroot/firebase-messaging-sw.js
@@ -1,7 +1,15 @@
 importScripts("https://www.gstatic.com/firebasejs/12.17.1/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/12.17.1/firebase-messaging-compat.js");
 
-firebase.initializeApp({ messagingSenderId: "1077893614286" });
+firebase.initializeApp({
+    apiKey: "AIzaSyBDxnZgDSspKUrcjdao39rfTL7PTgoW1DU",
+    authDomain: "doctory-1aca1.firebaseapp.com",
+    projectId: "doctory-1aca1",
+    storageBucket: "doctory-1aca1.firebasestorage.app",
+    messagingSenderId: "1077893614286",
+    appId: "1:1077893614286:web:be3f460b60b1dd290c9e9f",
+    measurementId: "G-FQGVDLQC6Q"
+});
 
 const messaging = firebase.messaging();
 
